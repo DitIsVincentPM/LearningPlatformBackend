04b0a167abe5a6e415bc4602481519a00ed376a0
DitIsVincentPM <vanhoofv4@gmail.com>
2024-09-16 17:08:41 +0200

    Added ability to change website origin with env

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 8080;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.WEBSITE_URL,
     methods: 'GET,POST',
     allowedHeaders: 'Content-Type, Authorization'
 };
